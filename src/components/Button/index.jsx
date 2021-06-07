178773912a5059ac7adfd9ce5b25b72b81c4ced9
Thiago Pereira <tp.maiochi@gmail.com>
2021-06-07 15:56:57 -0300

    Finalizando Modulo 1

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,19 +1,13 @@
 import './styles.css';
 
-import { Component } from "react";
-
-export  class Button extends Component {
-    render() {
-        const { text, actionFn, disabled } = this.props;
-
-        return (
-            <button 
-                disabled={disabled}
-                className="button" 
-                onClick={actionFn}
-            >
-                {text}
-            </button>
-        );
-    }
+export  const Button = ({  text, onClick, disabled }) => {
+    return (
+        <button 
+            disabled={disabled}
+            className="button" 
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    );
 }
